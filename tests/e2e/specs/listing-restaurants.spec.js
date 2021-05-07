ab0d66e4ac2774656e2d387c8ae7343abaf7696b
Eleri <elviks@ttu.ee>
2021-05-07 16:49:39 +0300

    Add RestaurantScreen and RestaurantList

--- tests/e2e/specs/listing-restaurants.spec.js
+++ tests/e2e/specs/listing-restaurants.spec.js
@@ -1,21 +1,22 @@
 describe('Listing Restaurants', () => {
-    it('shows restaurants from the server', () => {
-      const sushiPlace = 'Sushi Place';
-      const pizzaPlace = 'Pizza Place';
-  
-      cy.server({force404: true});
-  
-      cy.route({
-        method: 'GET',
-        url: 'https://outside-in-dev-api.herokuapp.com/16ZyiVYbUhNnCBmyGvgR73ZWIUM3xn5R/restaurants',
-        response: [
-          {id: 1, name: sushiPlace},
-          {id: 2, name: pizzaPlace},
-        ],
-      });
-  
-      cy.visit('/');
-      cy.contains(sushiPlace);
-      cy.contains(pizzaPlace);
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+
+    cy.server({force404: true});
+
+    cy.route({
+      method: 'GET',
+      url:
+        'https://outside-in-dev-api.herokuapp.com/PP4wXOuq2B8IVELqL6MinvgflXEGw1A6/restaurants',
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
     });
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
   });
+});
