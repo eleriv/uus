cc0ab38d2f02fcdaddfd2477a71aa94dd559fc88
Eleri <elviks@ttu.ee>
2021-05-07 17:45:28 +0300

    Server errors

--- tests/unit/store/restaurants.spec.js
+++ tests/unit/store/restaurants.spec.js
@@ -149,4 +149,12 @@ describe('create action', () => {
       return expect(promise).resolves.toBeUndefined();
     });
   });
+
+  describe('when save fails', () => {
+    it('rejects', () => {
+      api.createRestaurant.mockRejectedValue();
+      promise = store.dispatch('restaurants/create', newRestaurantName);
+      return expect(promise).rejects.toBeUndefined();
+    });
+  });
 });
